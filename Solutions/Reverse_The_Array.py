f3ac2d6d29fc772958f41ff5247df3411e181355
Rohit Jadhav <34981708+Rohit177@users.noreply.github.com>
2023-09-11 15:16:37 +0530

    Update Reverse_The_Array.py

--- Solutions/Reverse_The_Array.py
+++ Solutions/Reverse_The_Array.py
@@ -1,1 +1,16 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def reverseArray(arr, pos):
+    if pos >= len(arr):
+        return arr  
+    part1 = arr[:pos+1]
+    part2 = arr[pos+1:]
+
+    part2.reverse()
+
+    return part1 + part2
+
 
